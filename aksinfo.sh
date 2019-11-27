f684f26dec61b97323153f2413ce8b8b536f573a
Chris Green <cgvoip@hotmail.com>
2019-11-27 15:17:46 -0600

    Update aksinfo.sh

--- aksinfo.sh
+++ aksinfo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo ""
-echo -e "\e[32mAKS Name Space"
+echo -e "\e[32mAKS Name Space\e[0m"
 kubectl get ns
 echo ""
-echo -e "\e[32mAKS Nodes"
+echo -e "\e[32mAKS Nodes\e[0m"
 kubectl get nodes
 echo ""
-echo -e "\e[32mAKS Pods"
+echo -e "\e[32mAKS Pods\e[0m"
 kubectl get Pods
 echo ""
-echo -e "\e[32mAKS Services"
+echo -e "\e[32mAKS Services\e[0m"
 kubectl get services
