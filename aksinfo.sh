3673097abb40fe674ca87c9fee4100cc5fdfd7a8
Chris Green <cgvoip@hotmail.com>
2019-11-27 15:05:02 -0600

    Update aksinfo.sh

--- aksinfo.sh
+++ aksinfo.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-echo "AKS Name Space"
+echo ""
+tput setaf 6; echo "AKS Name Space"
 kubectl get ns
 echo ""
-echo "AKS Nodes"
+tput setaf 6; echo "AKS Nodes"
 kubectl get nodes
 echo ""
-echo "AKS Pods"
+tput setaf 6; echo "AKS Pods"
 kubectl get Pods
 echo ""
-echo "AKS Nodes"
-echo ""
-echo "AKS Services"
+tput setaf 6; echo "AKS Services"
 kubectl get services
